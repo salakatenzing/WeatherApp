6c445137d5835dd911b83443f7f15142b10d5fd9
Tenzing Salaka <salakatenzing@gmail.com>
2022-07-21 00:49:50 -0400

    functionality done

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,39 @@ import fetch from "node-fetch";
 const app = express();
 const port = 5000;
 
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const defaultMessage = 'Please Enter Location';
+  res.send(defaultMessage);
 });
 
 app.get("/getWeatherNYC", async (req, res) => {
   const response = await fetch(
-    "http://api.weatherstack.com/current?access_key=2ce4071c84675bb7b1394699a47570d2&query=New York"
+    "http://api.weatherapi.com/v1/current.json?key=1aa56e24515049f1a2201759221807&q=New York&aqi=no"
   );
   const body = await response.text();
   const parsedBody = JSON.parse(body);
-  const temp = parsedBody.current["temperature"];
+  const temp = parsedBody["current"]["temp_f"];
+  res.send(temp.toString());
+});
 
-  res.send({ temp });
+//`http://api.weatherapi.com/v1/current.json?key=1aa56e24515049f1a2201759221807&q=${location}&aqi=no`
+app.get("/:id", async (req, res) => {
+  const location = req.params.id;
+  console.log(location)
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=1aa56e24515049f1a2201759221807&q=${req.params.id}&aqi=no`
+  );
+  const body = await response.text();
+  const parsedBody = JSON.parse(body);
+  let temp = '';
+  for(let key in parsedBody.current){
+    if(key === 'temp_f'){
+      temp = parsedBody.current[key].toString();
+    }
+  }
+  res.send(temp);
+
 });
 
 app.listen(port, () => {
